845cebcbf03abb4537b210290138582f67ca1ba9
Sruthi-Palle <pallesruthi12@gmail.com>
2023-03-15 18:41:38 +0530

    remove gitignore files

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+const server = () => {
+  app.listen(PORT, () => {
+    console.log("listening to port:", PORT);
+  });
+};
+server();
