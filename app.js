209be52296dc39b7a1e678a067fcbd32e1263a97
Sruthi-Palle <pallesruthi12@gmail.com>
2023-03-18 13:28:10 +0530

    created controllers, modals and routers

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
+//read files in the directory
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+const { readdirSync } = require("fs");
+const PORT = 4000;
 const app = express();
 
-require("dotenv").config();
-
-const PORT = process.env.PORT;
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
 //middlewares
 app.use(express.json());
 app.use(cors());
 
-const server = () => {
-  app.listen(PORT, () => {
-    console.log("listening to port:", PORT);
-  });
-};
-server();
+//routes
+readdirSync("./routes").map((route) =>
+  app.use("/api/v1", require("./routes/" + route))
+);
+mongoose
+  .connect(
+    "mongodb://sruthipalle:frontend123@ac-6jshomw-shard-00-00.pt3egs8.mongodb.net:27017,ac-6jshomw-shard-00-01.pt3egs8.mongodb.net:27017,ac-6jshomw-shard-00-02.pt3egs8.mongodb.net:27017/?ssl=true&replicaSet=atlas-ybqs7j-shard-0&authSource=admin&retryWrites=true&w=majority"
+    // "mongodb+srv://sruthipalle:frontend123@expenseapp.pt3egs8.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("mongodb connection is successful"))
+  .catch((err) => console.log(err));
+app.get("/", () => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running at ${PORT}`);
+});
---
+++ routes/transaction.js
@@ -0,0 +1,20 @@
+const {
+  addExpense,
+  getExpense,
+  deleteExpense,
+} = require("../controllers/expense");
+const {
+  addIncome,
+  deleteIncome,
+  getIncomes,
+} = require("../controllers/income");
+
+const router = require("express").Router();
+router
+  .post("/add-income", addIncome)
+  .get("/get-incomes", getIncomes)
+  .delete("/delete-income/:id", deleteIncome)
+  .post("/add-expense", addExpense)
+  .get("/get-expenses", getExpense)
+  .delete("/delete-expense/:id", deleteExpense);
+module.exports = router;
